0965ced71cf6b9c32f718a90aa0c22e1de90f7ec
unseok.kim <unseok.kim@linecorp.com>
2022-02-24 23:29:17 +0900

    add Pageable

--- src/main/java/com/meteor/app/repo/custom/CustomRepository.java
+++ src/main/java/com/meteor/app/repo/custom/CustomRepository.java
@@ -2,6 +2,9 @@ package com.meteor.app.repo.custom;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 
 import com.meteor.app.entity.Book;
@@ -11,4 +14,10 @@ public interface CustomRepository {
     Book save(Book book);
 
     Optional<Book> findById(Long id);
+
+    @Query(value = "select * from Item where item_id=?1", nativeQuery = true)
+    Optional<Book> findByIdCustom(Long id);
+
+    @Query(value = "select * from Item", nativeQuery = true, countQuery = "select count(*) from item")
+    Page<Book> findAllPaging(Pageable pageable);
 }
--- src/test/java/com/meteor/app/repo/custom/CustomRepositoryTest.java
+++ src/test/java/com/meteor/app/repo/custom/CustomRepositoryTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.meteor.app.entity.Book;
 
@@ -28,5 +30,58 @@ public class CustomRepositoryTest {
         Optional<Book> byId = customRepository.findById(id);
 
         Assertions.assertThat(byId).get().matches(bok -> name.equals(bok.getName()) && price == bok.getPrice());
+    }
+
+    @Test
+    void findByIdCustom() {
+        Long id = 1L;
+        int price = 100;
+        String name = "name";
+
+        Book book = new Book();
+        book.setPrice(price);
+        book.setName(name);
+        customRepository.save(book);
+
+        Optional<Book> byId = customRepository.findByIdCustom(id);
+
+        Assertions.assertThat(byId).get().matches(bok -> name.equals(bok.getName()) && price == bok.getPrice());
+    }
+
+    @Test
+    void findAllPaging() {
+        int price = 100;
+        String name = "name";
+
+        int size = 100;
+        for (int i = 0; i < size; i++) {
+            Book book = new Book();
+            book.setPrice(price);
+            book.setName(name);
+            customRepository.save(book);
+        }
+
+        Pageable unpaged = Pageable.unpaged();
+        Page<Book> list = customRepository.findAllPaging(unpaged);
+        Assertions.assertThat(list).hasSize(size);
+    }
+
+    @Test
+    void findAllPagingFirst() {
+        int price = 100;
+        String name = "name";
+
+        int size = 100;
+        for (int i = 0; i < size; i++) {
+            Book book = new Book();
+            book.setPrice(price);
+            book.setName(name);
+            customRepository.save(book);
+        }
+
+        int perSize = 10;
+        Pageable first = Pageable.ofSize(perSize);
+        Page<Book> list = customRepository.findAllPaging(first);
+        Assertions.assertThat(list).hasSize(perSize);
     }
 }
